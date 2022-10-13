9725644afd511a796b8d9cbd0f2f5290385a2c87
Xianzhe ID <115347633+Xianzheid@users.noreply.github.com>
2022-10-13 23:29:10 +0700

    Add files via upload

--- command/random/asupan.js
+++ command/random/asupan.js
@@ -10,7 +10,7 @@ module.exports = {
       const asu = JSON.parse(fs.readFileSync('./lib/storage/asupan.json'))
       randIndex = Math.floor(Math.random() * asu.length);
       supan = asu[randIndex];
-      await conn.sendMessage(msg.from, {video: {url : supan.url},caption: global.footer},{quoted: msg,})
+      await conn.sendMessage(msg.from, {image: {url : supan.url},caption: global.footer},{quoted: msg,})
     } catch (e){
       global.error(cmdNya, e, msg)
     }
